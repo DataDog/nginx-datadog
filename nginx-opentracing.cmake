7434464594e95b01fce24dfb2f8b5955fa558525
David Goffredo <dmgoffredo@gmail.com>
2021-08-06 20:53:11 -0400

    playing with env forwarding and JSON-ish parsing

--- nginx-opentracing.cmake
+++ nginx-opentracing.cmake
@@ -14,9 +14,11 @@ target_sources(nginx_opentracing
         nginx-opentracing/opentracing/src/ngx_http_opentracing_module.cpp
         nginx-opentracing/opentracing/src/opentracing_context.cpp
         nginx-opentracing/opentracing/src/span_context_querier.cpp
+        nginx-opentracing/opentracing/src/ngx_filebuf.cpp
         nginx-opentracing/opentracing/src/ngx_script.cpp
         nginx-opentracing/opentracing/src/extract_span_context.cpp
         nginx-opentracing/opentracing/src/request_tracing.cpp
+        nginx-opentracing/opentracing/src/config_util.cpp
         nginx-opentracing/opentracing/src/utility.cpp
         nginx-opentracing/opentracing/src/opentracing_directive.cpp
         nginx-opentracing/opentracing/src/opentracing_variable.cpp
