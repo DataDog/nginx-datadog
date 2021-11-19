3522ae42594f1c1d95050cd4dddb437d0d7d3f21
David Goffredo <david.goffredo@datadoghq.com>
2021-11-19 21:59:50 +0000

    document datadog_defer.h

--- src/datadog_defer.h
+++ src/datadog_defer.h
@@ -2,15 +2,54 @@
 
 #include <utility>
 
-// TODO: document
+// This component provides a class template, `CleanupFuncGuard`, together with
+// a utility function, `defer`, that allow for deferred execution of arbitrary
+// code at the end of a lexical scope.
+//
+// For example, the following code restores the value of `conf.cmd` anywhere
+// the enclosing function returns or throws an exception ("!"), but only if
+// `guard` has been initialized ("←").
+//
+//     int inject_config(Config* conf, const char* old_name, const char* new_name) try {
+//         if (std::strcmp(old_name, new_name) == 0) {
+//             return 0;
+//         }
+//     
+//         const auto *const old_command = conf.cmd;
+//         std::vector<const char*> new_command = *old_command;
+//         new_command[0] = new_name;
+//         complicated_initialization(&new_command);
+//         
+//         conf.cmd = &new_command;
+//         const auto guard = defer([&]() { conf.cmd = old_command; }); // ←
+//     
+//         if (const int rc = dispatch_config(conf) /* ! */) {
+//             log_error(rc.message());
+//             return rc.code(); // !
+//         }
+//
+//         /* more code here ... */ // !    
+// 
+//         return 0; // !
+//     } catch (const LibraryError& error) {
+//         log_error(error.what());
+//         return error.code(); // !
+//     }
+//
+// The function template `defer` returns an object that, when destroyed,
+// invokes the function-like object passed to `defer`.
+
 namespace datadog {
 namespace nginx {
 
-// TODO: document
+// This class template invokes a specified function-like object in its
+// destructor.  Moving from a `CleanupFuncGuard` disables this behavior, and
+// the type is move-only, so the function-like object will not be invoked more
+// than once. 
 template <typename Func>
 class CleanupFuncGuard {
     Func on_destroy_;
-    bool active_;
+    bool active_; // whether to call `on_destroy_` in the destructor
 
   public:
     explicit CleanupFuncGuard(Func&& func)
@@ -32,7 +71,7 @@ class CleanupFuncGuard {
     }
 };
 
-// TODO: document
+// Return a guard object that invokes the specified `func` when destroyed.
 template <typename Func>
 CleanupFuncGuard<Func> defer(Func&& func) {
     return CleanupFuncGuard<Func>(std::forward<Func>(func));
