c0d033f0e096d37a42235eec61ebce39c1607bfd
David Goffredo <david.goffredo@datadoghq.com>
2022-03-19 00:04:22 +0000

    tests about warnings about deprecated opentracing_* directives

--- test/cases/deprecations/test_deprecation_warnings.py
+++ test/cases/deprecations/test_deprecation_warnings.py
@@ -7,6 +7,34 @@ class TestDeprecationWarnings(cases.case.TestCase):
     def test_opentracing_trace_locations(self):
         directive = 'opentracing_trace_locations'
         return self.run_test_for_config(f'conf/{directive}.conf', directive)
+    
+    def test_opentracing_propagate_context(self):
+        directive = 'opentracing_propagate_context'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_fastcgi_propagate_context(self):
+        directive = 'opentracing_fastcgi_propagate_context'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_grpc_propagate_context(self):
+        directive = 'opentracing_grpc_propagate_context'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_operation_name(self):
+        directive = 'opentracing_operation_name'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_location_operation_name(self):
+        directive = 'opentracing_location_operation_name'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_trust_incoming_span(self):
+        directive = 'opentracing_trust_incoming_span'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
+
+    def test_opentracing_tag(self):
+        directive = 'opentracing_tag'
+        return self.run_test_for_config(f'conf/{directive}.conf', directive)
 
     def run_test_for_config(self, config_relative_path, directive):
         config_path = Path(__file__).parent / config_relative_path
