cd8f27f116a0790cc5649094a55b997d9df13963
David Goffredo <david.goffredo@datadoghq.com>
2022-08-03 21:26:41 +0000

    CI: apt-get upgrade to get more recent certificate authorities

--- bin/install_build_tooling_apt.sh
+++ bin/install_build_tooling_apt.sh
@@ -13,13 +13,18 @@ install() {
 }
 
 # for downloading releases of build dependencies, unpacking them, and building them
-install build-essential libtool autoconf unzip wget tar curl git
+install build-essential libtool autoconf unzip wget tar curl git ca-certificates
 
 # nproc, etc.
 install coreutils
 
 # nginx uses perl-compatible regular expressions (PCRE) and zlib (for gzip).
 install libpcre3-dev zlib1g-dev
+
+# Older debians will have out of date certificate authorities, which will cause
+# wget to fail.  This heavy-handed update prevents that.
+apt-get update
+DEBIAN_FRONTENT=noninteractive apt-get upgrade --yes
 
 # Build a recent cmake from source.  We need at least 3.12, but ubuntu 18
 # packages 3.10.
