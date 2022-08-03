61e3e070532b53c17ae05aa94da55ee024f536fa
David Goffredo <david.goffredo@datadoghq.com>
2022-08-03 20:22:42 +0000

    CI: Add a "build and test all job", but it runs all the time...

---
+++ bin/generate_jobs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Print out a snippet that will go in .circleci/config.yaml
+#
+# It describes a "build" and "test" job for each nginx tag in release.json
+
+indentation='    '
+
+REPO=$(dirname "$(dirname "$(realpath "$0")")")
+cd "$REPO"
+
+<release.json jq --raw-output .nginx_tags[] | { while read nginx_tag || [ -n "$nginx_tag" ]; do
+    cat <<END_SNIPPET
+- build:
+    name: "build $nginx_tag"
+    build-image: "datadog/docker-library:nginx-datadog-build-$nginx_tag"
+    nginx-tag: "$nginx_tag"
+- test:
+    name: "test $nginx_tag"
+    requires:
+    - "build $nginx_tag"
+END_SNIPPET
+done } | sed "s/^/$indentation/"
