5b95cd25c7e40bda0f0537263213d663856e2c74
David Goffredo <david.goffredo@datadoghq.com>
2022-08-10 19:30:08 +0000

    working release script; needs some docs

--- bin/release.py
+++ bin/release.py
@@ -3,14 +3,31 @@
 """
 
 import argparse
+import itertools
 import json
 import os
+from pathlib import Path
 import re
 import shlex
 import shutil
 import subprocess
 import sys
+import tempfile
+import time
 import urllib.request
+
+
+class VerboseDict(dict):
+    """This makes debugging a little easier."""
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError as error:
+            # In addition to the missing key, print the object itself.
+            message = f'{repr(key)} is not in {self}'
+            error.args = message,
+            raise error
 
 
 class MissingDependency(Exception):
@@ -53,6 +70,15 @@ def get_git():
     return exe_path
 
 
+def get_tar():
+    exe_path = shutil.which('tar')
+    if exe_path is None:
+        raise MissingDependency(
+            'The "tar" command must be available to compress and archive the nginx module.'
+        )
+    return exe_path
+
+
 def validate_version_tag(tag):
     pattern = r'v[0-9]+\.[0-9]+\.[0-9]+'
     if re.fullmatch(pattern, tag) is None:
@@ -83,12 +109,13 @@ def parse_options(args):
 
 
 try:
+    options = parse_options(sys.argv[1:])
+    options.version_tag = validate_version_tag(options.version_tag)
     ci_api_token = get_token()
     gh_exe = get_gh()
     gpg_exe = get_gpg()
     git_exe = get_git()
-    options = parse_options(sys.argv[1:])
-    options.version_tag = validate_version_tag(options.version_tag)
+    tar_exe = get_tar()
 except (MissingDependency, ValueError) as error:
     print(str(error), file=sys.stderr)
     sys.exit(1)
@@ -105,6 +132,7 @@ print({
 
 
 def run(command, *args, **kwargs):
+    command = [str(arg) for arg in command]
     print('+', shlex.join(command), file=sys.stderr)
     return subprocess.run(command, *args, **kwargs)
 
@@ -127,8 +155,6 @@ if not options.no_tag:
     command = [git_exe, 'push', options.remote, options.version_tag]
     run(command, check=True)
 
-PROJECT_URL = 'https://circleci.com/api/v2/project/gh/DataDog/nginx-datadog'
-
 
 def send_ci_request(path, payload=None, method=None):
     headers = {'Circle-Token': ci_api_token}
@@ -136,44 +162,174 @@ def send_ci_request(path, payload=None, method=None):
         headers['Content-Type'] = 'application/json; charset=utf-8'
         payload = json.dumps(payload).encode('utf8')
 
-    url = f'{PROJECT_URL}{path}'
+    API_URL = 'https://circleci.com/api/v2'
+    url = f'{API_URL}{path}'
     request = urllib.request.Request(url,
                                      data=payload,
                                      headers=headers,
                                      method=method)
     print('+', request.get_method(), request.full_url, request.data or '')
 
-    response = urllib.request.urlopen(request)
-    response_body = json.load(response)
-    if response.status < 200 or response.status > 299:
-        raise Exception(
-            f'HTTP error response {response.status}: {response_body}')
-    return response.status, response_body
+    try:
+        response = urllib.request.urlopen(request)
+        status = response.status
+    except urllib.error.HTTPError as error:
+        response = error
+        status = error.code
 
+    try:
+        response_body = json.load(response, object_hook=VerboseDict)
+    except Exception as error:
+        print(
+            f'Unable to parse response body from response with status {status}.'
+        )
+        raise
+
+    if status < 200 or status > 299:
+        raise Exception(f'HTTP error response {status}: {response_body}')
+
+    return status, response_body
+
+
+PROJECT_SLUG = 'gh/DataDog/nginx-datadog'
 
 # Kick off a CircleCI pipeline.
 if options.pipeline_id is not None:
     pipeline_id = options.pipeline_id
 else:
     body = {"tag": options.version_tag}
-    _, response = send_ci_request('/pipeline', payload=body, method='POST')
+    _, response = send_ci_request(f'/project/{PROJECT_SLUG}/pipeline',
+                                  payload=body,
+                                  method='POST')
+    print(response)
     pipeline_id = response.get('id')
     if pipeline_id is None:
         raise Exception(
             f'POST [...]/pipeline response did not contain pipeline "id": {response}'
         )
 
-# Fetch the pipeline's information.  This will contain its jobs.
-_, response = send_ci_request(f'/pipeline/{pipeline_id}')
-print(response)
 
-# TODO
+def send_ci_request_paged(path, payload=None, method=None):
+    items = []
+    query = ''
+    while True:
+        _, response = send_ci_request(f'{path}{query}',
+                                      payload=payload,
+                                      method=method)
+        items += response['items']
+
+        next_page = response.get('next_page_token')
+        if next_page is None:
+            break
+        query = f'?page-token={next_page}'
+
+    return items
+
+
+# Fetch the pipeline's information.  This will contain its workflows.
+# We're looking for exactly one "build-and-test-all" workflow.
+workflows = send_ci_request_paged(f'/pipeline/{pipeline_id}/workflow')
+workflow = [wf for wf in workflows if wf['name'] == 'build-and-test-all']
+if len(workflow) != 1:
+    raise Exception(
+        f'Workflows contains the wrong number of "build-and-test-all".  Expected 1 but got {len(workflow)}: {workflows}'
+    )
+workflow = workflow[0]
+workflow_id = workflow['id']
+
+
+def download_file(url, destination):
+    response = urllib.request.urlopen(url)
+    with open(destination, 'wb') as output:
+        shutil.copyfileobj(response, output)
+
+
+def prepare_release_artifact(build_job_number, work_dir):
+    artifacts = send_ci_request_paged(
+        f'/project/{PROJECT_SLUG}/{build_job_number}/artifacts')
+    nginx_tag_url = None
+    module_url = None
+    for artifact in artifacts:
+        name = artifact['path']
+        if name == 'nginx-tag':
+            nginx_tag_url = artifact['url']
+        elif name == 'ngx_http_datadog_module.so':
+            module_url = artifact['url']
+
+    if nginx_tag_url is None:
+        raise Exception(
+            f"Job number {build_job_number} doesn't have an 'nginx-tag' build artifact."
+        )
+    if module_url is None:
+        raise Exception(
+            f"Job number {build_job_number} doesn't have an 'ngx_http_datadog_module.so' build artifact."
+        )
+
+    nginx_tag = urllib.request.urlopen(nginx_tag_url).read().decode('utf8')
+    module_path = work_dir / 'ngx_http_datadog_module.so'
+    download_file(module_url, module_path)
+
+    tarball_path = work_dir / f'{nginx_tag}-ngx_http_datadog_module.so.tgz'
+    command = [tar_exe, '-czf', tarball_path, '-C', work_dir, module_path.name]
+    run(command, check=True)
+
+    command = [gpg_exe, '--armor', '--detach-sign', tarball_path]
+    run(command, check=True)
+
+    module_path.unlink()
+
+
+def handle_job(job, work_dir):
+    # See the response schema for a list of statuses:
+    # https://circleci.com/docs/api/v2/index.html#operation/listWorkflowJobs
+    status = job['status']
+    if status in ('not_run', 'failed', 'infrastructure_fail', 'timedout',
+                  'terminated-unknown', 'canceled', 'unauthorized'):
+        raise Exception(f'Job has fatal status "{status}": {job}')
+    elif status == 'success':
+        if job['name'].startswith('build '):
+            prepare_release_artifact(job['job_number'], work_dir)
+        return 'done'
+
+
+work_dir = Path(tempfile.mkdtemp())
+print('Working directory is', work_dir)
+
+# Now get the jobs for the workflow.  Each job will be either a build or a test
+# of a build.
 #
-# x tag the current head
-# - kick off pipeline
-# - get workflows
-# - extrace "build-and-test-all" workflow
-# - poll workflow's jobs until all are successful
-# - for each build job, download artifacts
-# - .tgz with GPG detached signature
-# - use "gh"
+# Each test depends on the success of a build, so we know that a build's
+# artifacts are kosher once the corresponding test has succeeded.
+#
+# TODO: Describe it better than that.
+done_jobs = set()  # job numbers (not IDs)
+while True:
+    jobs = send_ci_request_paged(f'/workflow/{workflow_id}/job')
+    for job in jobs:
+        job_number = job['job_number']
+        if job_number not in done_jobs:
+            result = handle_job(job, work_dir)
+            if result == 'done':
+                done_jobs.add(job_number)
+
+    if len(done_jobs) == len(jobs):
+        break
+
+    sleep_seconds = 20
+    print(
+        f'Going to sleep for {sleep_seconds} seconds before checking jobs again.'
+    )
+    time.sleep(sleep_seconds)
+
+# We've tgz'd and signed all of our release modules.
+# Now let's send them to GitHub in a release via `gh release create`.
+release_files = itertools.chain(work_dir.glob('*.tgz'),
+                                work_dir.glob('*.tgz.asc'))
+command = [
+    gh_exe, 'release', 'create', '--prerelease', '--draft', '--title',
+    options.version_tag, '--notes', 'TODO', options.version_tag, *release_files
+]
+run(command, check=True)
+
+print('removing', work_dir)
+shutil.rmtree(work_dir)
