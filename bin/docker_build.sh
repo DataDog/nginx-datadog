3c6976572c0b00697f57a55eb1733c22f1f9d32b
David Goffredo <david.goffredo@datadoghq.com>
2023-01-31 15:45:22 -0500

    I pushed some build images

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -x
 set -e
 
 usage() {
@@ -9,7 +8,7 @@ docker_build.sh - Create a build image in Docker
 
 usage:
 
-    docker_build.sh [--yes] [--push] [<BASE_IMAGE>]
+    docker_build.sh [--yes] [--push] [--rm] [<BASE_IMAGE>]
         `docker build` an image suitable for building a Datadog nginx
         module compatible with the optionally specified BASE_IMAGE.
         If BASE_IMAGE is not specified, use the contents of the
@@ -20,6 +19,9 @@ usage:
         If --push is specified, push the resulting image to DockerHub
         with a suitable tag.
 
+        If --rm is specified, then the image will be deleted locally after
+        it's been build and/or pushed.
+
     docker_build.sh --help
     docker_build.sh -h
         Print this message.
@@ -29,6 +31,7 @@ END_USAGE
 repo=$(dirname "$0")/..
 yes=0
 push=0
+delete=0
 base_image=''
 
 while [ $# -ne 0 ]; do
@@ -36,6 +39,7 @@ while [ $# -ne 0 ]; do
     -h|--help) usage; exit ;;
     -y|--yes) yes=1 ;;
     -p|--push) push=1 ;;
+    -d|--rm) delete=1 ;;
     *)
         if [ -n "$base_image" ]; then
             >&2 printf 'base image was specified twice: first as %s and now as %s.' "$base_image" "$1"
@@ -84,3 +88,7 @@ fi
 
 docker tag "$built_tag" "$destination"
 docker push "$destination"
+
+if [ "$delete" -eq 1 ]; then
+    docker images --no-trunc | awk "{ if (\$2 == \"$built_tag\") { print \$3 } }" | xargs docker rmi --force
+fi
