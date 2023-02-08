f9288ad4e6030f76103d2b5d5ef18dd0f80718c8
David Goffredo <david.goffredo@datadoghq.com>
2023-02-08 14:15:00 -0500

    working on adding example/ to CircleCI

--- example/services/nginx/install_datadog.sh
+++ example/services/nginx/install_datadog.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+set -x
 set -e
 
 if [ "$BASE_IMAGE" = '' ]; then
   >&2 echo 'This script expects $BASE_IMAGE to be in the environment, e.g. "nginx:1.23.1-alpine" or "amazonlinux:2.0.20220121.0".'
   exit 1
 fi
+
+case "$BASE_IMAGE" in
+    amazonlinux:*) nginx_modules_path=/usr/share/nginx/modules ;;
+    *) nginx_modules_path=/usr/lib/nginx/modules ;;
+esac
 
 # If nginx itself is not installed already, then install it.
 if ! command -v nginx >/dev/null 2>&1; then
@@ -51,5 +57,5 @@ RELEASE_TAG=$(get_latest_release DataDog/nginx-datadog)
 base_image_without_colons=$(echo "$BASE_IMAGE" | tr ':' '_')
 tarball="$base_image_without_colons-ngx_http_datadog_module.so.tgz"
 wget "https://github.com/DataDog/nginx-datadog/releases/download/$RELEASE_TAG/$tarball"
-tar -xzf "$tarball" -C /usr/lib/nginx/modules
+tar -xzf "$tarball" -C "$nginx_modules_path"
 rm "$tarball"
