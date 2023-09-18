3a6b6a7550c5fe917ac421e5f51da970a86f361f
Damien Mehala <damien.mehala@datadoghq.com>
2023-09-18 17:08:50 +0200

    wip: quick fix

--- test/services/client/install-tools.sh
+++ test/services/client/install-tools.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
 set -e
+
+ARCH="$(uname -m)"
 
 apk update
 apk add wget tar curl jq
 
 # grpcurl is a self-contained binary (Go program)
 cd /tmp
-wget https://github.com/fullstorydev/grpcurl/releases/download/v1.8.6/grpcurl_1.8.6_linux_x86_64.tar.gz
-tar -xzf grpcurl_1.8.6_linux_x86_64.tar.gz
+wget https://github.com/fullstorydev/grpcurl/releases/download/v1.8.6/grpcurl_1.8.6_linux_${ARCH}.tar.gz
+tar -xzf grpcurl_1.8.6_linux_${ARCH}.tar.gz
 mv grpcurl /usr/local/bin
-rm grpcurl_1.8.6_linux_x86_64.tar.gz
+rm grpcurl_1.8.6_linux_${ARCH}.tar.gz
