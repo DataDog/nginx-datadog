d44423809193d659a511e218a057b7d5c7434ae9
Damien Mehala <damien.mehala@datadoghq.com>
2023-11-29 00:03:14 +0100

    chore: add architecture in release process (#51)
    * chore: add architecture in release process
    
    * Code review
    
    - Describe the purpose of `ARCH` in `release.py` and `nginx-version-info.example`
    
    * more review: touch up arch-related documentation
    
    * revert submodule change
    
    ---------
    
    Co-authored-by: David Goffredo <david.goffredo@datadoghq.com>

--- bin/release.py
+++ bin/release.py
@@ -288,16 +288,22 @@ def prepare_release_artifact(build_job_number, work_dir):
     module_path = work_dir / 'ngx_http_datadog_module.so'
     download_file(module_url, module_path)
 
-    # `nginx_version_info` will define BASE_IMAGE. That's that value that we
-    # want to prefix the tarball name with, but with colons replaced by
-    # underscores.
+    # `nginx_version_info` serves to determine the values of BASE_IMAGE and ARCH.
+    # These values are instrumental in constructing the tarball name according to
+    # the specified convention: <BASE_IMAGE>-<ARCH>-ngx_http_datadog_module.so.tgz
     variables = parse_info_script(nginx_version_info)
     if 'BASE_IMAGE' not in variables:
         raise Exception(
             f"BASE_IMAGE not found in nginx-version-info: {nginx_version_info}"
         )
+    if 'ARCH' not in variables:
+        raise Exception(
+            f"ARCH not found in nginx-version-info: {nginx_version_info}"
+        )
+
+    arch = variables['ARCH']
     base_prefix = variables['BASE_IMAGE'].replace(':', '_')
-    tarball_path = work_dir / f'{base_prefix}-ngx_http_datadog_module.so.tgz'
+    tarball_path = work_dir / f'{base_prefix}-{arch}-ngx_http_datadog_module.so.tgz'
     command = [tar_exe, '-czf', tarball_path, '-C', work_dir, module_path.name]
     run(command, check=True)
 
