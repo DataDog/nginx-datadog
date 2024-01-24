75b1eb347d2153d5e9ec232ea12224c0bc4d2c3d
David Goffredo <david.goffredo@datadoghq.com>
2024-01-24 15:16:20 -0500

    lint.sh: error status on bad formatting, but run all of them

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
-# Lint the codebase and returns all encountered discrepencies
-# with our codestyle
+# Print any discrepancies between the formatting of the code and the expected
+# style.
+
+error_messages=''
 
 find src/ -type f \( -name '*.h' -o -name '*.cpp' \) -print0 | xargs -0 clang-format-14 --Werror --dry-run --style=file
+rc=$?
+if [ "$rc" -ne 0 ]; then
+    error_messages=$(printf '%s\nC++ formatter reported formatting differences in src/ and returned error status %d.\n' "$error_messages" "$rc")
+fi
+
 find bin/ -type f -name '*.py' -print0 | xargs -0 yapf3 --diff
+rc=$?
+if [ "$rc" -ne 0 ]; then
+    error_messages=$(printf '%s\nPython formatter reported formatting differences in bin/ and returned error status %d.\n' "$error_messages" "$rc")
+fi
 
 yapf3 --recursive --diff "$@" "test/"
+rc=$?
+if [ "$rc" -ne 0 ]; then
+    error_messages=$(printf '%s\nPython formatter reported formatting differences in test/ and returned error status %d.\n' "$error_messages" "$rc")
+fi
+
+if [ -n "$error_messages" ]; then
+    >&2 echo "$error_messages"
+    exit 1
+fi
