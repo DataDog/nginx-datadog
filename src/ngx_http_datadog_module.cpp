f4de84d4a7c515d30b5fce862f716fdb33b54b95
Damien Mehala <damien.mehala@datadoghq.com>
2024-06-03 13:38:09 +0200

    feat: add RUM SDK injection

--- src/ngx_http_datadog_module.cpp
+++ src/ngx_http_datadog_module.cpp
@@ -18,9 +18,12 @@
 #include "defer.h"
 #include "global_tracer.h"
 #include "ngx_logger.h"
-#ifdef WITH_WAF
+#if defined(WITH_WAF)
 #include "security/library.h"
 #include "security/waf_remote_cfg.h"
+#endif
+#if defined(WITH_RUM)
+#include "rum/config.h"
 #endif
 #include "string_util.h"
 #include "tracing_library.h"
@@ -46,6 +49,10 @@ static char *merge_datadog_loc_conf(ngx_conf_t *, void *parent, void *child) noe
 // clang-format on
 
 using namespace datadog::nginx;
+
+#ifndef DATADOG_RUM_DIRECTIVES
+#define DATADOG_RUM_DIRECTIVES
+#endif
 
 // Each "datadog_*" directive has a corresponding "opentracing_*" alias that
 // logs a warning and then delegates to the "datadog_*" version, e.g.
@@ -381,7 +388,7 @@ static ngx_command_t datadog_commands[] = {
       nullptr,
     },
 #endif
-
+    DATADOG_RUM_DIRECTIVES
     ngx_null_command
 };
 
@@ -506,6 +513,12 @@ static ngx_int_t datadog_master_process_post_config(
   ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0, "- tracing: dd-trace-cpp@%s",
                 datadog_version_tracer);
 
+#ifdef WITH_RUM
+  ngx_log_error(NGX_LOG_INFO, cycle->log, 0,
+                "- rum-injection: inject-browser-sdk@%s",
+                datadog_semver_rum_injector);
+#endif
+
   // Forward tracer-specific environment variables to worker processes.
   for (const auto &env_var_name :
        TracingLibrary::environment_variable_names()) {
@@ -530,8 +543,6 @@ static ngx_int_t datadog_master_process_post_config(
   ngx_log_error(NGX_LOG_NOTICE, cycle->log, 0,
                 "- appsec: libddwaf@%s, waf_rules@%s", datadog_semver_libddwaf,
                 datadog_semver_waf_rules);
-  ngx_http_next_output_body_filter = ngx_http_top_body_filter;
-  ngx_http_top_body_filter = output_body_filter;
 #endif
 
   // Forward tracer-specific environment variables to worker processes.
@@ -545,6 +556,7 @@ static ngx_int_t datadog_master_process_post_config(
        TracingLibrary::environment_variable_names()) {
     push_to_main_conf(std::string{env_var_name});
   }
+
 #ifdef WITH_WAF
   for (const std::string_view &env_var_name :
        security::Library::environment_variable_names()) {
@@ -556,6 +568,12 @@ static ngx_int_t datadog_master_process_post_config(
 }
 
 static ngx_int_t datadog_module_init(ngx_conf_t *cf) noexcept {
+  ngx_http_next_header_filter = ngx_http_top_header_filter;
+  ngx_http_top_header_filter = on_header_filter;
+
+  ngx_http_next_output_body_filter = ngx_http_top_body_filter;
+  ngx_http_top_body_filter = on_output_body_filter;
+
   auto core_main_config = static_cast<ngx_http_core_main_conf_t *>(
       ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module));
   auto main_conf = static_cast<datadog_main_conf_t *>(
@@ -700,6 +718,11 @@ static void *create_datadog_loc_conf(ngx_conf_t *conf) noexcept {
     return nullptr;  // error
   }
 
+#ifdef WITH_RUM
+  loc_conf->rum_enable = NGX_CONF_UNSET;
+  loc_conf->rum_snippet = nullptr;
+#endif
+
   return loc_conf;
 }
 
@@ -825,6 +848,10 @@ static char *merge_datadog_loc_conf(ngx_conf_t *cf, void *parent,
   if (conf->waf_pool == nullptr) {
     conf->waf_pool = prev->waf_pool;
   }
+#endif
+
+#ifdef WITH_RUM
+  datadog_rum_merge_loc_config(cf, prev, conf);
 #endif
 
   return NGX_CONF_OK;
