DataDog/nginx-datadog

commit 02e66068da2a914e41710f38401c02d8963d9ce5

refactor: tracing injection mechanism (#79)
This commit introduces a refined header injection mechanism that modify
the request structure with headers to inject, instead of mutating nginx's
configuration with `proxy_set_header`, `fastcgi_param`, `grpc_set_header`
or `uwsgi_param`.

Resolves #19
Resolves #43

Changes:
  - Add a DictWriter implementation for nginx.
  - Add a test covering issue #19.
  - Deprecate `opentracing_*_propagate_context`.
  - Remove tests no longer necessary.
  - Update existing tests to align with the new implementation.

src/ngx_header_writer.h
@@ -1,0 +1,56 @@
+#pragma once
+
+#include <datadog/dict_writer.h>
+
+#include "string_util.h"
+
+extern "C" {
+#include <nginx.h>
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+}
+
+namespace datadog {
+namespace nginx {
+
+class NgxHeaderWriter : public datadog::tracing::DictWriter {
+  ngx_http_request_t *request_;
+  ngx_pool_t *pool_;
+
+ public:
+  explicit NgxHeaderWriter(ngx_http_request_t *request)
+      : request_(request), pool_(request_->pool) {}
+
+  void set(std::string_view key, std::string_view value) override {
+    const auto key_size = key.size();
+
+    ngx_table_elt_t *h =
+        (ngx_table_elt_t *)ngx_list_push(&request_->headers_in.headers);
+    if (h == NULL) {
+      return;
+    }
+
+    // This trick tells ngx_http_header_module to reflect the header value
+    // in the actual response. Otherwise the header will be ignored and client
+    // will never see it. To date the value must be just non zero.
+    // Source:
+    // <https://www.nginx.com/resources/wiki/start/topics/examples/headers_management/>
+    h->hash = 1;
+
+    // HTTP proxy module expects the header to has a lowercased key value
+    // Instead of allocating twice the same key, `h->key` and `h->lowcase_key`
+    // use the same data.
+    h->key.data = (u_char *)ngx_pnalloc(pool_, sizeof(char) * key_size);
+    h->key.len = key_size;
+    for (std::size_t i = 0; i < key_size; ++i) {
+      h->key.data[i] = to_lower(key[i]);
+    }
+    h->lowcase_key = h->key.data;
+
+    h->value = to_ngx_str(pool_, value);
+  }
+};
+
+}  // namespace nginx
+}  // namespace datadog

test/cases/auto_propagation/test_http.py
@@ -7,22 +7,40 @@ from pathlib import Path
 class TestHTTP(case.TestCase):
 
     def test_auto_propagation(self):
-        return self.run_test('./conf/http_auto.conf', should_propagate=True)
+        conf_path = Path(__file__).parent / "./conf/http_auto.conf"
+        conf_text = conf_path.read_text()
+        status, log_lines = self.orch.nginx_replace_config(
+            conf_text, conf_path.name)
+        self.assertEqual(status, 0, log_lines)
+
+        status, _, body = self.orch.send_nginx_http_request("/http")
+        self.assertEqual(status, 200)
+        response = json.loads(body)
+        self.assertEqual(response["service"], "http")
+        headers = response["headers"]
+
+        self.assertIn("server-block-header", headers)
+        self.assertEqual("not-hidden-by-autoinjection",
+                         headers["server-block-header"])
+
+        self.assertIn("x-datadog-sampling-priority", headers)
+        priority = headers["x-datadog-sampling-priority"]
+        priority = int(priority)
 
     def test_disabled_at_location(self):
-        return self.run_test('./conf/http_disabled_at_location.conf',
+        return self.run_test("./conf/http_disabled_at_location.conf",
                              should_propagate=False)
 
     def test_disabled_at_server(self):
-        return self.run_test('./conf/http_disabled_at_server.conf',
+        return self.run_test("./conf/http_disabled_at_server.conf",
                              should_propagate=False)
 
     def test_disabled_at_http(self):
-        return self.run_test('./conf/http_disabled_at_http.conf',
+        return self.run_test("./conf/http_disabled_at_http.conf",
                              should_propagate=False)
 
     def test_without_module(self):
-        return self.run_test('./conf/http_without_module.conf',
+        return self.run_test("./conf/http_without_module.conf",
                              should_propagate=False)
 
     def run_test(self, conf_relative_path, should_propagate):
@@ -32,14 +50,14 @@ class TestHTTP(case.TestCase):
             conf_text, conf_path.name)
         self.assertEqual(status, 0, log_lines)
 
-        status, _, body = self.orch.send_nginx_http_request('/http')
+        status, _, body = self.orch.send_nginx_http_request("/http")
         self.assertEqual(status, 200)
         response = json.loads(body)
-        self.assertEqual(response['service'], 'http')
-        headers = response['headers']
+        self.assertEqual(response["service"], "http")
+        headers = response["headers"]
         if should_propagate:
-            self.assertIn('x-datadog-sampling-priority', headers)
-            priority = headers['x-datadog-sampling-priority']
+            self.assertIn("x-datadog-sampling-priority", headers)
+            priority = headers["x-datadog-sampling-priority"]
             priority = int(priority)
         else:
-            self.assertNotIn('x-datadog-sampling-priority', headers)
+            self.assertNotIn("x-datadog-sampling-priority", headers)

CMakeLists.txt
@@ -94,7 +94,6 @@ target_sources(ngx_http_datadog_module
     src/ngx_http_datadog_module.cpp
     src/ngx_logger.cpp
     src/ngx_script.cpp
-    src/propagation_header_querier.cpp
     src/request_tracing.cpp
     src/string_util.cpp
     src/tracing_library.cpp

nginx-module.cmake
@@ -10,9 +10,13 @@ separate_arguments(NGINX_CONF_ARGS NATIVE_COMMAND "${NGINX_CONF_ARGS}")
 
 list(APPEND NGINX_CONF_ARGS
   "--add-dynamic-module=${CMAKE_SOURCE_DIR}/module/"
-  "--with-threads"
   "--with-compat"
 )
+
+if (NGINX_DATADOG_ASM_ENABLED)
+  list(APPEND NGINX_CONF_ARGS "--with-threads")
+endif ()
+
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
   list(APPEND NGINX_CONF_ARGS "--with-debug")
 endif()

src/datadog_conf.h
@@ -86,7 +86,6 @@ struct datadog_main_conf_t {
   // (e.g. before the first `server` block, before the first `access_log`
   // directive).
   bool are_log_formats_defined;
-  std::vector<std::string_view> span_context_keys;
   // This module automates the forwarding of the environment variables in
   // `TracingLibrary::environment_variable_names()`. Rather than injecting
   // `env` directives into the configuration, or mucking around with the core
@@ -109,13 +108,6 @@ struct datadog_main_conf_t {
   std::optional<configured_value_t> environment;
   // `agent_url` is set by the `datadog_agent_url` directive.
   std::optional<configured_value_t> agent_url;
-  //  `is_sampling_delegation_response_header_added` is whether we have added an
-  //  `add_header` directive before the first `server` block within the
-  //  configuration. The directive is used to include the
-  //  X-Datadog-Trace-Sampling-Decision response header when the client
-  //  requested trace sampling delegation. This `bool` ensures that the
-  //  directive is not added more than once.
-  bool is_sampling_delegation_response_header_added;
 
 #ifdef WITH_WAF
   // DD_APPSEC_ENABLED
@@ -196,10 +188,6 @@ struct datadog_loc_conf_t {
   NgxScript loc_resource_name_script;
   ngx_flag_t trust_incoming_span = NGX_CONF_UNSET;
   ngx_array_t *tags;
-  // `response_info_script` is a script containing variables that refer
-  // to HTTP response headers.  This is used by sampling delegation to
-  // retrieve the sampling decision made by an upstream service.
-  NgxScript response_info_script;
   // `proxy_directive` is the name of the configuration directive used to proxy
   // requests at this location, i.e. `proxy_pass`, `grpc_pass`, or
   // `fastcgi_pass`.  If this location does not have such a directive directly
@@ -226,15 +214,6 @@ struct datadog_loc_conf_t {
   // `sampling_delegation_directive` is the source location of the
   // `datadog_delegate_sampling` directive that applies this location, if any.
   conf_directive_source_location_t sampling_delegation_directive;
-  //  `is_sampling_delegation_response_header_added` is whether we have added an
-  //  `add_header` directive before the first user-specified `add_header`
-  //  directive within this server/location block. The directive is used to
-  //  include the X-Datadog-Trace-Sampling-Decision response header when the
-  //  client requested trace sampling delegation. The inserted `add_header` is
-  //  necessary only if there are user-defined `add_header` directives at the
-  //  same level; if not, then the `add_header` is inherited from the `http`
-  //  block.
-  bool is_sampling_delegation_response_header_added;
   // `block_type` is the name of the kind of configuration block we're in, e.g.
   // "http", "server", "location", or "if". `block_type` is used by some
   // configuration directives to alter their behavior based on the current

src/datadog_context.cpp
@@ -86,17 +86,6 @@ void DatadogContext::on_log_request(ngx_http_request_t *request) {
 #endif
 }
 
-ngx_str_t DatadogContext::lookup_propagation_header_variable_value(
-    ngx_http_request_t *request, std::string_view key) {
-  auto trace = find_trace(request);
-  if (trace == nullptr) {
-    throw std::runtime_error{
-        "lookup_propagation_header_variable_value failed: could not find "
-        "request trace"};
-  }
-  return trace->lookup_propagation_header_variable_value(key);
-}
-
 ngx_str_t DatadogContext::lookup_span_variable_value(
     ngx_http_request_t *request, std::string_view key) {
   auto trace = find_trace(request);
@@ -105,17 +94,6 @@ ngx_str_t DatadogContext::lookup_span_variable_value(
         "lookup_span_variable_value failed: could not find request trace"};
   }
   return trace->lookup_span_variable_value(key);
-}
-
-ngx_str_t DatadogContext::lookup_sampling_delegation_response_variable_value(
-    ngx_http_request_t *request) {
-  auto trace = find_trace(request);
-  if (trace == nullptr) {
-    throw std::runtime_error{
-        "lookup_sampling_delegation_response_variable_value failed: could not "
-        "find request trace"};
-  }
-  return trace->lookup_sampling_delegation_response_variable_value();
 }
 
 RequestTracing *DatadogContext::find_trace(ngx_http_request_t *request) {

src/datadog_context.h
@@ -6,7 +6,6 @@
 #include <vector>
 
 #include "datadog_conf.h"
-#include "propagation_header_querier.h"
 #include "request_tracing.h"
 #ifdef WITH_WAF
 #include "security/context.h"
@@ -41,14 +40,8 @@ class DatadogContext {
 
   void on_log_request(ngx_http_request_t* request);
 
-  ngx_str_t lookup_propagation_header_variable_value(
-      ngx_http_request_t* request, std::string_view key);
-
   ngx_str_t lookup_span_variable_value(ngx_http_request_t* request,
                                        std::string_view key);
-
-  ngx_str_t lookup_sampling_delegation_response_variable_value(
-      ngx_http_request_t* request);
 
   RequestTracing& single_trace();
 

commit a8a0b10dff8213117d63a912df179c78b2de7a8c

fix: propagation header value adding up
This commit fix a regression introduce in #79.
During the context propagation phase, if an HTTP header already exist,
the value is appended instead of being overwrite.

Changes:
  - Add integration test covering the issue.
  - Update header injection to overwrite the value for an existing header name.

src/ngx_header_writer.h
@@ -25,30 +25,62 @@ class NgxHeaderWriter : public datadog::tracing::DictWriter {
   void set(std::string_view key, std::string_view value) override {
     const auto key_size = key.size();
 
-    ngx_table_elt_t *h =
-        (ngx_table_elt_t *)ngx_list_push(&request_->headers_in.headers);
-    if (h == NULL) {
-      return;
+    ngx_table_elt_t *h = search_header(key);
+    if (h != nullptr) {
+      h->value = to_ngx_str(pool_, value);
+    } else {
+      h = static_cast<ngx_table_elt_t *>(
+          ngx_list_push(&request_->headers_in.headers));
+      if (h == nullptr) {
+        return;
+      }
+
+      // This trick tells ngx_http_header_module to reflect the header value
+      // in the actual response. Otherwise the header will be ignored and client
+      // will never see it. To date the value must be just non zero.
+      // Source:
+      // <https://web.archive.org/web/20240409072840/https://www.nginx.com/resources/wiki/start/topics/examples/headers_management/>
+      h->hash = 1;
+
+      // HTTP proxy module expects the header to has a lowercased key value
+      // Instead of allocating twice the same key, `h->key` and `h->lowcase_key`
+      // use the same data.
+      h->key.len = key_size;
+      h->key.data = (u_char *)ngx_pnalloc(pool_, sizeof(char) * key_size);
+      for (std::size_t i = 0; i < key_size; ++i) {
+        h->key.data[i] = to_lower(key[i]);
+      }
+      h->lowcase_key = h->key.data;
+
+      h->value = to_ngx_str(pool_, value);
+    }
+  }
+
+ private:
+  ngx_table_elt_t *search_header(std::string_view key) {
+    ngx_list_part_t *part = &request_->headers_in.headers.part;
+    auto *h = static_cast<ngx_table_elt_t *>(part->elts);
+
+    for (std::size_t i = 0;; i++) {
+      if (i >= part->nelts) {
+        if (part->next == nullptr) {
+          break;
+        }
+
+        part = part->next;
+        h = static_cast<ngx_table_elt_t *>(part->elts);
+        i = 0;
+      }
+
+      if (key.size() != h[i].key.len ||
+          ngx_strcasecmp((u_char *)key.data(), h[i].key.data) != 0) {
+        continue;
+      }
+
+      return &h[i];
     }
 
-    // This trick tells ngx_http_header_module to reflect the header value
-    // in the actual response. Otherwise the header will be ignored and client
-    // will never see it. To date the value must be just non zero.
-    // Source:
-    // <https://www.nginx.com/resources/wiki/start/topics/examples/headers_management/>
-    h->hash = 1;
-
-    // HTTP proxy module expects the header to has a lowercased key value
-    // Instead of allocating twice the same key, `h->key` and `h->lowcase_key`
-    // use the same data.
-    h->key.data = (u_char *)ngx_pnalloc(pool_, sizeof(char) * key_size);
-    h->key.len = key_size;
-    for (std::size_t i = 0; i < key_size; ++i) {
-      h->key.data[i] = to_lower(key[i]);
-    }
-    h->lowcase_key = h->key.data;
-
-    h->value = to_ngx_str(pool_, value);
+    return nullptr;
   }
 };
 

test/cases/auto_propagation/test_http.py
@@ -27,6 +27,32 @@ class TestHTTP(case.TestCase):
         priority = headers["x-datadog-sampling-priority"]
         priority = int(priority)
 
+    def test_context_extraction(self):
+        conf_path = Path(__file__).parent / "./conf/http_auto.conf"
+        conf_text = conf_path.read_text()
+        status, log_lines = self.orch.nginx_replace_config(
+            conf_text, conf_path.name)
+        self.assertEqual(status, 0, log_lines)
+
+        tracing_context_headers = {
+            "x-datadog-trace-id": "2993963891409991723",
+            "x-datadog-parent-id": "6383613330463382713",
+        }
+
+        status, _, body = self.orch.send_nginx_http_request(
+            "/http", headers=tracing_context_headers)
+        self.assertEqual(status, 200)
+        response = json.loads(body)
+        self.assertEqual(response["service"], "http")
+        headers = response["headers"]
+
+        self.assertEqual(tracing_context_headers["x-datadog-trace-id"],
+                         headers["x-datadog-trace-id"])
+        self.assertNotEqual(
+            tracing_context_headers["x-datadog-parent-id"],
+            headers["x-datadog-parent-id"],
+        )
+
     def test_disabled_at_location(self):
         return self.run_test("./conf/http_disabled_at_location.conf",
                              should_propagate=False)