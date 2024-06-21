bfe543eaf1982eee5cf3c011aac1fa689a8e96da
Damien Mehala <damien.mehala@datadoghq.com>
2024-06-21 12:58:48 +0200

    add NGINX 1.27.0

---
+++ bin/generate_jobs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Print out a snippet that will go in .circleci/config.yaml
+# It describes a "build" and "test" job for each supported nginx tag.
+
+import sys
+
+if __name__ == "__main__":
+    sys.exit(0)
