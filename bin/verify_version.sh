9a20afdb5b66ca1c07ee18a81a79108fa82a8fb2
Gustavo Lopes <mail@geleia.net>
2024-11-19 16:32:02 +0000

    Bump version to 1.4.0

--- bin/verify_version.sh
+++ bin/verify_version.sh
@@ -4,7 +4,7 @@ function main {
   readonly expected_mod_version=$1
 
   local mod_version=
-  mod_version=$(grep 'project(.*VERSION' CMakeLists.txt  | sed 's/.* \([0-9.]\+\)).*/\1/')
+  mod_version=$(sed -n 's/set(NGINX_DATADOG_VERSION\s\+\([0-9.]\+\))/\1/p' CMakeLists.txt)
 
   if [[ $expected_mod_version != "$mod_version" ]]; then
     echo "datadog_version_ngx_mod mismatch: $expected_mod_version != $mod_version" >&2
