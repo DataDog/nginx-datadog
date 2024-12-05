19dfb153531d0152d62a66189f68191f555a4cf8
Pablo Martínez Bernardo <pablo.martinezbernardo@datadoghq.com>
2024-12-05 15:41:21 +0100

    RUM module is not yet built with datadog_tracing directive

--- installer/configurator/nginx_configurator.go
+++ installer/configurator/nginx_configurator.go
@@ -264,7 +264,7 @@ func transformConfig(n *NginxConfigurator, content []byte, agentUri string, appI
 	datadogConfig := fmt.Sprintf(`    datadog_agent_url %s;
 
     # Disable APM Tracing. Remove the next line to enable APM Tracing.
-    datadog_tracing off;
+    datadog_disable;
 
     # Enable RUM Injection
     datadog_rum on;
