b12a1edf09d8988331b02ddbedb47320c5c94a9b
Damien Mehala <damien.mehala@datadoghq.com>
2024-12-05 11:22:13 +0100

    chore: remove support for old NGINX versions

--- bin/generate_jobs_yaml.rb
+++ bin/generate_jobs_yaml.rb
@@ -2,25 +2,6 @@
 
 version_table = <<-TAB
 amazonlinux:2023.3.20240219.0 1.24.0
-amazonlinux:2.0.20230418.0 1.22.1
-amazonlinux:2.0.20230320.0 1.22.1
-amazonlinux:2.0.20230307.0 1.22.1
-amazonlinux:2.0.20230221.0 1.22.1
-amazonlinux:2.0.20230207.0 1.22.1
-amazonlinux:2.0.20230119.1 1.22.1
-amazonlinux:2.0.20221210.0 1.22.1
-amazonlinux:2.0.20221103.3 1.22.1
-amazonlinux:2.0.20221004.0 1.22.1
-amazonlinux:2.0.20220912.1 1.22.1
-amazonlinux:2.0.20220805.0 1.22.1
-amazonlinux:2.0.20220719.0 1.22.1
-amazonlinux:2.0.20220606.1 1.22.1
-amazonlinux:2.0.20220426.0 1.22.1
-amazonlinux:2.0.20220419.0 1.22.1
-amazonlinux:2.0.20220406.1 1.22.1
-amazonlinux:2.0.20220316.0 1.22.1
-amazonlinux:2.0.20220218.1 1.22.1
-amazonlinux:2.0.20220121.0 1.22.1
 nginx:1.27.2-alpine 1.27.2
 nginx:1.27.2 1.27.2
 nginx:1.27.1-alpine 1.27.1
@@ -47,20 +28,6 @@ nginx:1.25.0-alpine 1.25.0
 nginx:1.25.0 1.25.0
 nginx:1.24.0-alpine 1.24.0
 nginx:1.24.0 1.24.0
-nginx:1.23.4-alpine 1.23.4
-nginx:1.23.4 1.23.4
-nginx:1.23.3-alpine 1.23.3
-nginx:1.23.3 1.23.3
-nginx:1.23.2-alpine 1.23.2
-nginx:1.23.2 1.23.2
-nginx:1.23.1-alpine 1.23.1
-nginx:1.23.1 1.23.1
-nginx:1.23.0-alpine 1.23.0
-nginx:1.23.0 1.23.0
-nginx:1.22.1-alpine 1.22.1
-nginx:1.22.1 1.22.1
-nginx:1.22.0-alpine 1.22.0
-nginx:1.22.0 1.22.0
 TAB
 
 SpecLine = Struct.new(:image, :version)
