1aadb4be81dd5935a90aedfdf5c7f96c1268e871
Damien Mehala <damien.mehala@datadoghq.com>
2025-08-28 23:31:18 +0200

    revert: af20c99b78c2f8b83d4b2bbb2507ab9197b37122 (#244)

--- src/datadog_variable.cpp
+++ src/datadog_variable.cpp
@@ -12,7 +12,6 @@
 #include "global_tracer.h"
 #include "ngx_http_datadog_module.h"
 #include "string_util.h"
-#include "telemetry_util.h"
 #include "tracing_library.h"
 
 namespace datadog {
@@ -73,7 +72,6 @@ static ngx_int_t expand_span_variable(ngx_http_request_t *request,
 
   return NGX_OK;
 } catch (const std::exception &e) {
-  telemetry::log::error(e.what(), CURRENT_FRAME(request));
   ngx_log_error(NGX_LOG_ERR, request->connection->log, 0,
                 "failed to expand %V"
                 " for request %p: %s",
