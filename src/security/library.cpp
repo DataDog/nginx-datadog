3334d6abb45d8e80bab2724698594821e0d39712
Robert Pickering <robert.pickering@datadoghq.com>
2025-09-01 15:26:10 +0200

    update obfuscation

--- src/security/library.cpp
+++ src/security/library.cpp
@@ -375,17 +375,18 @@ namespace datadog::nginx::security {
 class FinalizedConfigSettings {
   static constexpr ngx_uint_t kDefaultWafTimeoutUsec = 1000000;  // 100 ms
   static constexpr std::string_view kDefaultObfuscationKeyRegex =
-      "(?i)(?:p(?:ass)?w(?:or)?d|pass(?:_?phrase)?|secret|(?:api_?|private_?|"
-      "public_?)key)|token|consumer_?(?:id|key|secret)|sign(?:ed|ature)|bearer|"
-      "authorization"sv;
+      R"((?i)pass|pw(?:or)?d|secret|(?:api|private|public|access)[_-]?key"
+      R"|token|consumer[_-]?(?:id|key|secret)|sign(?:ed|ature)|bearer"
+      R"|authorization|jsessionid|phpsessid|asp\.net[_-]sessionid|sid|jwt)";
   static constexpr std::string_view kDefaultObfuscationValueRegex =
-      R"((?i)(?:p(?:ass)?w(?:or)?d|pass(?:_?phrase)?|secret|(?:api_?|private_?)"
-      R"(|public_?|access_?|secret_?)key(?:_?id)?|token|consumer_?(?:id|key|)"
-      R"(secret)|sign(?:ed|ature)?|auth(?:entication|orization)?)(?:\s*=[^;]|")"
-      R"(\s*:\s*"[^"]+")|bearer\s+[a-z0-9._-]+|token:[a-z0-9]{13}|gh[opsu]_)"
-      R"([0-9a-zA-Z]{36}|ey[I-L][\w=-]+\.ey[I-L][\w=-]+(?:\.[\w.+/=-]+)?|)"
-      R"([-]{5}BEGIN[a-z\s]+PRIVATE\sKEY[-]{5}[^-]+[-]{5}END[a-z\s]+PRIVATE\s)"
-      R"(KEY|ssh-rsa\s*[a-z0-9/.+]{100,})";
+      R"((?i)(?:p(?:ass)?w(?:or)?d|pass(?:[_-]?phrase)?|secret(?:[_-]?key)?"
+      R"|(?:(?:api|private|public|access)[_-]?)key(?:[_-]?id)?
+      R"|(?:(?:auth|access|id|refresh)[_-]?)?token|consumer[_-]?(?:id|key|secret)
+      R"|sign(?:ed|ature)?|auth(?:entication|orization)?|jsessionid|phpsessid|
+      R"asp\.net(?:[_-]|-)sessionid|sid|jwt)(?:\s*=([^;&]+)|"\s*:\s*("[^"]+"|\d+))
+      R"|bearer\s+([a-z0-9\._\-]+)|token\s*:\s*([a-z0-9]{13})|gh[opsu]_([0-9a-zA-Z]{36})
+      R"|ey[I-L][\w=-]+\.(ey[I-L][\w=-]+(?:\.[\w.+\/=-]+)?)|[\-]{5}BEGIN[a-z\s]+
+      R"PRIVATE\sKEY[\-]{5}([^\-]+)[\-]{5}END[a-z\s]+PRIVATE\sKEY|ssh-rsa\s*([a-z0-9\/\.+]{100,}))";
 
  public:
   enum class enable_status : std::uint8_t {
