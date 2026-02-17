1b649713fa22a9fd499e28d9a79665e9ad7ab7d0
Paweł Chojnacki <pawelchcki@gmail.com>
2026-02-17 14:25:32 +0100

    remove deps/inject-browser-sdk

--- deps/inject-browser-sdk/envoy-lua-filter/watch_units.sh
+++
@@ -1,8 +0,0 @@
-# Unless explicitly stated otherwise all files in this repository are licensed
-# under the Apache 2.0 License. This product includes software developed at
-# Datadog (https://www.datadoghq.com/).
-#
-# Copyright 2024-Present Datadog, Inc.
-
-fswatch -0 -o --recursive . | xargs -0 -n1 -I{} busted -v --suppress-pending
-
