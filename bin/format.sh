4b5955d36fceca22d91297714e2f8f66ad0262fd
Xavier Lamorlette <xavier.lamorlette@datadoghq.com>
2026-09-15 20:08:03 +0200

    Format Python code in test folder

--- bin/format.sh
+++ bin/format.sh
@@ -12,6 +12,6 @@ if ! [ -e .clang-format ]; then
 fi
 
 find src/ test/ tools/ -type f \( -name '*.h' -o -name '*.cpp' -o -name '*.c' \) -print0 | xargs -0 clang-format-14 -i --style=file
-find bin/ -type f -name '*.py' -print0 | xargs -0 yapf -i
+find bin/ test/ -type f -name '*.py' -print0 | xargs -0 yapf -i
 
 yapf --recursive --in-place "$@" "test/"
